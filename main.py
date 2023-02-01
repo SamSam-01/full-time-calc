7107a90e2feb0e5662d1ad32a2f392469b1decb0
Samuel Blard <sam.blard974.sb@gmail.com>
2023-02-01 17:51:52 +0100

    main takes 0 or 1 arguments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from sys import argv
 from parse_file import parse, print_entreprise_info, print_entreprises
 from full_time_calc import coefficients_equivalent_temps_plein
 from datetime import date
@@ -11,11 +12,12 @@ date_sortie = date(2019, 12, 21) #date de sortie de l'entreprise
 date_input = date(2019, 12, 28) #date de calcul
 
 def main():
-    coefficients = coefficients_equivalent_temps_plein(periodes_temps_partiel, date_entree, date_sortie, date_input)
-    print(coefficients)
-    print("-" * 80)
-    parse("Test.txt")
-    print_entreprise_info(1)
+    if (len(argv) < 2):
+        coefficients = coefficients_equivalent_temps_plein(periodes_temps_partiel, date_entree, date_sortie, date_input)
+        print(coefficients)
+    else:
+        parse(argv[1])
+        print_entreprise_info(1)
 
 if __name__ == "__main__":
     main()
